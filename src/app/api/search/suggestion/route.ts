f31f001e8e521510f6ab1b00d46959c20bfa6a4a
BFG14 <70485453+BFG14@users.noreply.github.com>
2025-03-12 07:53:19 +0500

    search url upd

--- src/app/api/search/suggestion/route.ts
+++ src/app/api/search/suggestion/route.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import { NextResponse } from 'next/server';
 
-const BASE_URI = "https://www1.gogoanime.ai";
+const BASE_URI = "https://anitaku.bz/";
 
 interface SuggestionItem {
   id: string | null;
